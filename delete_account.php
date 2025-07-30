cd14f479fa2fb17b8f94d96dcd205b670a9c775d
Kim613-bit <phanthang07bn@gmail.com>
2025-07-30 17:10:41 +0700

    Update delete_account.php

--- delete_account.php
+++ delete_account.php
@@ -12,18 +12,16 @@ if (isset($_GET['account_id'])) {
     $user_id = $_SESSION['user_id'];
 
     // Xoá tất cả giao dịch liên quan đến tài khoản này
-    $delete_transactions_sql = "DELETE FROM transactions WHERE account_id = ?";
-    $stmt_transactions = mysqli_prepare($conn, $delete_transactions_sql);
-    mysqli_stmt_bind_param($stmt_transactions, "i", $account_id);
-    mysqli_stmt_execute($stmt_transactions);
-    mysqli_stmt_close($stmt_transactions);
+    pg_query_params($conn,
+        "DELETE FROM transactions WHERE account_id = $1",
+        [$account_id]
+    );
 
     // Xoá tài khoản khỏi bảng accounts
-    $delete_account_sql = "DELETE FROM accounts WHERE id = ? AND user_id = ?";
-    $stmt_account = mysqli_prepare($conn, $delete_account_sql);
-    mysqli_stmt_bind_param($stmt_account, "ii", $account_id, $user_id);
-    mysqli_stmt_execute($stmt_account);
-    mysqli_stmt_close($stmt_account);
+    pg_query_params($conn,
+        "DELETE FROM accounts WHERE id = $1 AND user_id = $2",
+        [$account_id, $user_id]
+    );
 }
 
 header("Location: dashboard.php");
