fcca8b6531f79ca7c78128316da69143b7bfe059
Kim613-bit <phanthang07bn@gmail.com>
2025-08-09 19:58:55 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -130,7 +130,6 @@ $avatarPath = 'uploads/' . (!empty($user['avatar']) ? $user['avatar'] : 'avt_mem
           z-index: 1000;
           background: var(--color-primary);
           color: white;
-          padding: 12px 24px;
           display: flex;
           justify-content: space-between;
           align-items: center;
